8caf7a881ac9b53e79c6b2f71cd12351bd898e00
jbermejo14 <jpbermejobramley@gmail.com>
2024-11-16 14:06:59 +0000

    Selected squares work and perfect movement on some pieces

--- board.py
+++ board.py
@@ -7,11 +7,14 @@ white = pygame.Color(255, 255, 255)
 gameDisplay = pygame.display.set_mode((1200, 800))
 gameExit = True
 
+selected_piece = None
 turn = 'white'
 
 bg = pygame.image.load('resources/img/bg.png')
 brownBoard = pygame.image.load('resources/img/light.png')
 lightBrownBoard = pygame.image.load('resources/img/dark.png')
+selectedBrownBoard = pygame.image.load('resources/img/dark_selected.png')
+selectedLightBrownBoard = pygame.image.load('resources/img/light_selected.png')
 w_pawn = pygame.image.load('resources/img/style1/w_pawn.png')
 w_rook = pygame.image.load('resources/img/style1/w_rook.png')
 w_knight = pygame.image.load('resources/img/style1/w_knight.png')
@@ -37,7 +40,6 @@ class Piece:
     moves = []
 
     def __init__(self, kind: str, color: str, img: Surface, pos: tuple):
-        self.selected = False
         self.kind = kind
         self.color = color
         self.img = img
@@ -50,20 +52,31 @@ class Piece:
         x, y = self.pos
         return x * square_size + 300, (7 - y) * square_size + 100  # Adjust for GUI positions
 
-    def selected_action(self, possible_moves):
-        print(self.kind)
 
-        for i in possible_moves:
-            a = main_dict.get(i)
-            if 0 <= i[0] < 8 and 0 <= i[1] < 8:
-                print(i)
-                print(a)
+    def selected_action(self, possible_moves):
+        def select_squares():
+            for square in square_list:
+                square.for_selection = False
+            for i in possible_moves:
+                a = main_dict.get(i)
+                if 0 <= i[0] < 8 and 0 <= i[1] < 8:
+                    for square in square_list:
+                        if square.has_piece is False:
+                            if square.pos == i:
+                                square.for_selection = True
+                                if square.color == 'brown':
+                                    square.img = selectedLightBrownBoard
+                                elif square.color == 'light':
+                                    square.img = selectedBrownBoard
+
+        select_squares()
 
         #   TODO
         #       IT SHOULD GET A LIST OF THE POSSIBLE MOVES
         #       PSEUDO:
         #           FROM "main_dict" GET THE POSSIBLE "get_possible_moves()"
         #           PAWN -> X, Y + 1..... ETC
+
     def __repr__(self):
         return f"Piece(name={self.kind!r}, color={self.color!r}, position={self.pos!r})"
 
@@ -149,7 +162,6 @@ class King(Piece):
             moves.append((x + i, y - i))  # Up-right
             moves.append((x - i, y + i))  # Down-left
             moves.append((x - i, y - i))  # Up-left
-
         return moves
 
 
@@ -160,15 +172,32 @@ class Pawn(Piece):
 
 
 class Square:
-
-    def __init__(self, pos: tuple, color: str):
+    def __init__(self, pos: tuple, color: str, has_piece: bool):
+        self.for_selection = False
+        self.has_piece = has_piece
         self.pos = pos
         self.color = color
         self.int_coords = self.get_gui_pos()
+        self.top_rect = pygame.Rect(self.int_coords, (75, 75))
+        if self.color == 'light':
+            self.img = lightBrownBoard
+        elif self.color == 'brown':
+            self.img = brownBoard
 
     def get_gui_pos(self, square_size=75):
         x, y = self.pos
         return x * square_size + 300, (7 - y) * square_size + 100  # Adjust for GUI positions
+
+    def selected_action(self):
+        if selected_piece is not None:
+            moves_list = selected_piece.get_possible_moves()
+            for move in moves_list:
+                if move == self.pos:
+                    selected_piece.pos = self.pos
+                    selected_piece.int_coords = self.int_coords
+                    selected_piece.top_rect = pygame.Rect(self.int_coords, (75, 75))
+                    pygame.display.update()
+                    print(selected_piece.pos)
 
 
 gameDisplay.fill(black)
@@ -210,79 +239,77 @@ bishop_w_2 = Bishop('Bishop', 'white', w_bishop, (5, 0))
 knight_w_2 = Knight('Knight', 'white', w_knight, (6, 0))
 rook_w_2 = Rook('Rook', 'white', w_rook, (7, 0))
 
+square1 = Square((0, 0), 'brown', True)
+square2 = Square((0, 1), 'light', True)
+square3 = Square((0, 2), 'brown', False)
+square4 = Square((0, 3), 'light', False)
+square5 = Square((0, 4), 'brown', False)
+square6 = Square((0, 5), 'light', False)
+square7 = Square((0, 6), 'brown', True)
+square8 = Square((0, 7), 'light', True)
 
-square1 = Square((0, 0), 'brown')
-square2 = Square((0, 1), 'light')
-square3 = Square((0, 2), 'brown')
-square4 = Square((0, 3), 'light')
-square5 = Square((0, 4), 'brown')
-square6 = Square((0, 5), 'light')
-square7 = Square((0, 6), 'brown')
-square8 = Square((0, 7), 'light')
+square9 = Square((1, 0), 'light', True)
+square10 = Square((1, 1), 'brown', True)
+square11 = Square((1, 2), 'light', False)
+square12 = Square((1, 3), 'brown', False)
+square13 = Square((1, 4), 'light', False)
+square14 = Square((1, 5), 'brown', False)
+square15 = Square((1, 6), 'light', True)
+square16 = Square((1, 7), 'brown', True)
 
-square9 = Square((1, 0), 'light')
-square10 = Square((1, 1), 'brown')
-square11 = Square((1, 2), 'light')
-square12 = Square((1, 3), 'brown')
-square13 = Square((1, 4), 'light')
-square14 = Square((1, 5), 'brown')
-square15 = Square((1, 6), 'light')
-square16 = Square((1, 7), 'brown')
+square17 = Square((2, 0), 'brown', True)
+square18 = Square((2, 1), 'light', True)
+square19 = Square((2, 2), 'brown', False)
+square20 = Square((2, 3), 'light', False)
+square21 = Square((2, 4), 'brown', False)
+square22 = Square((2, 5), 'light', False)
+square23 = Square((2, 6), 'brown', True)
+square24 = Square((2, 7), 'light', True)
 
-square17 = Square((2, 0), 'brown')
-square18 = Square((2, 1), 'light')
-square19 = Square((2, 2), 'brown')
-square20 = Square((2, 3), 'light')
-square21 = Square((2, 4), 'brown')
-square22 = Square((2, 5), 'light')
-square23 = Square((2, 6), 'brown')
-square24 = Square((2, 7), 'light')
+square25 = Square((3, 0), 'light', True)
+square26 = Square((3, 1), 'brown', True)
+square27 = Square((3, 2), 'light', False)
+square28 = Square((3, 3), 'brown', False)
+square29 = Square((3, 4), 'light', False)
+square30 = Square((3, 5), 'brown', False)
+square31 = Square((3, 6), 'light', True)
+square32 = Square((3, 7), 'brown', True)
 
-square25 = Square((3, 0), 'light')
-square26 = Square((3, 1), 'brown')
-square27 = Square((3, 2), 'light')
-square28 = Square((3, 3), 'brown')
-square29 = Square((3, 4), 'light')
-square30 = Square((3, 5), 'brown')
-square31 = Square((3, 6), 'light')
-square32 = Square((3, 7), 'brown')
+square33 = Square((4, 0), 'brown', True)
+square34 = Square((4, 1), 'light', True)
+square35 = Square((4, 2), 'brown', False)
+square36 = Square((4, 3), 'light', False)
+square37 = Square((4, 4), 'brown', False)
+square38 = Square((4, 5), 'light', False)
+square39 = Square((4, 6), 'brown', True)
+square40 = Square((4, 7), 'light', True)
 
-square33 = Square((4, 0), 'brown')
-square34 = Square((4, 1), 'light')
-square35 = Square((4, 2), 'brown')
-square36 = Square((4, 3), 'light')
-square37 = Square((4, 4), 'brown')
-square38 = Square((4, 5), 'light')
-square39 = Square((4, 6), 'brown')
-square40 = Square((4, 7), 'light')
+square41 = Square((5, 0), 'light', True)
+square42 = Square((5, 1), 'brown', True)
+square43 = Square((5, 2), 'light', False)
+square44 = Square((5, 3), 'brown', False)
+square45 = Square((5, 4), 'light', False)
+square46 = Square((5, 5), 'brown', False)
+square47 = Square((5, 6), 'light', True)
+square48 = Square((5, 7), 'brown', True)
 
-square41 = Square((5, 0), 'light')
-square42 = Square((5, 1), 'brown')
-square43 = Square((5, 2), 'light')
-square44 = Square((5, 3), 'brown')
-square45 = Square((5, 4), 'light')
-square46 = Square((5, 5), 'brown')
-square47 = Square((5, 6), 'light')
-square48 = Square((5, 7), 'brown')
+square49 = Square((6, 0), 'brown', True)
+square50 = Square((6, 1), 'light', True)
+square51 = Square((6, 2), 'brown', False)
+square52 = Square((6, 3), 'light', False)
+square53 = Square((6, 4), 'brown', False)
+square54 = Square((6, 5), 'light', False)
+square55 = Square((6, 6), 'brown', True)
+square56 = Square((6, 7), 'light', True)
 
-square49 = Square((6, 0), 'brown')
-square50 = Square((6, 1), 'light')
-square51 = Square((6, 2), 'brown')
-square52 = Square((6, 3), 'light')
-square53 = Square((6, 4), 'brown')
-square54 = Square((6, 5), 'light')
-square55 = Square((6, 6), 'brown')
-square56 = Square((6, 7), 'light')
-
-square57 = Square((7, 0), 'light')
-square58 = Square((7, 1), 'brown')
-square59 = Square((7, 2), 'light')
-square60 = Square((7, 3), 'brown')
-square61 = Square((7, 4), 'light')
-square62 = Square((7, 5), 'brown')
-square63 = Square((7, 6), 'light')
-square64 = Square((7, 7), 'brown')
-
+square57 = Square((7, 0), 'light', True)
+square58 = Square((7, 1), 'brown', True)
+square59 = Square((7, 2), 'light', False)
+square60 = Square((7, 3), 'brown', False)
+square61 = Square((7, 4), 'light', False)
+square62 = Square((7, 5), 'brown', False)
+square63 = Square((7, 6), 'light', True)
+square64 = Square((7, 7), 'brown', True)
 
 square_list = [square1, square2, square3, square4, square5, square6, square7, square8, square9,
                square10, square11, square12, square13, square14, square15, square16, square17,
@@ -311,40 +338,56 @@ main_dict = {
     (6, 7): knight_b_2, (7, 7): rook_b_2
 }
 
-
-
-for i in square_list:
-    if i.color == 'light':
-        gameDisplay.blit(brownBoard, (i.int_coords[0], i.int_coords[1], 10, 10))
-    elif i.color == 'brown':
-        gameDisplay.blit(lightBrownBoard, (i.int_coords[0], i.int_coords[1], 10, 10))
-
 whites = [pawn_w_1, pawn_w_2, pawn_w_3, pawn_w_4, pawn_w_5, pawn_w_6, pawn_w_7, pawn_w_8,
           rook_w_1, rook_w_2, knight_w_1, knight_w_2, bishop_w_1, bishop_w_2, queen_w, king_w]
 blacks = [pawn_b_1, pawn_b_2, pawn_b_3, pawn_b_4, pawn_b_5, pawn_b_6, pawn_b_7, pawn_b_8,
           rook_b_1, rook_b_2, knight_b_1, knight_b_2, bishop_b_1, bishop_b_2, queen_b, king_b]
 
-for i in blacks:
-    gameDisplay.blit(i.img, (i.int_coords[0], i.int_coords[1], 10, 10))
-
-for i in whites:
-    gameDisplay.blit(i.img, (i.int_coords[0], i.int_coords[1], 10, 10))
-
-pygame.display.update()
-
 
 def check_click(click_pos):  # Checks which piece has been pressed
+    global selected_piece
     # if turn == 'white':
     for w_piece in whites:
         if w_piece.top_rect.collidepoint(click_pos):
-            w_piece.selected = True
+            selected_piece = w_piece
             w_piece.selected_action(w_piece.get_possible_moves())
 
     # elif turn == 'black':
     for b_piece in blacks:
         if b_piece.top_rect.collidepoint(click_pos):
-            b_piece.selected = True
+            selected_piece = b_piece
             b_piece.selected_action(b_piece.get_possible_moves())
+
+    for square in square_list:
+        if square.has_piece is False:
+            if square.top_rect.collidepoint(click_pos):
+                for i in square_list:
+                    i.selected = False
+                square.selected = True
+                square.selected_action()
+
+
+def refresh_screen():
+    for square in square_list:
+        # If square IS for selection (in the piece movement) it has the 'for selection'
+        if square.for_selection:
+            gameDisplay.blit(square.img, (square.int_coords[0], square.int_coords[1], 10, 10))
+
+        # If square IS NOT for selection (in the piece movement) it has the normal img
+        elif not square.for_selection:
+            if square.color == 'light':
+                square.img = lightBrownBoard
+            elif square.color == 'brown':
+                square.img = brownBoard
+
+            gameDisplay.blit(square.img, (square.int_coords[0], square.int_coords[1], 10, 10))
+
+    for i in blacks:
+        gameDisplay.blit(i.img, (i.int_coords[0], i.int_coords[1], 10, 10))
+    for i in whites:
+        gameDisplay.blit(i.img, (i.int_coords[0], i.int_coords[1], 10, 10))
+
+    pygame.display.update()
 
 
 # Main loop
@@ -355,3 +398,4 @@ while gameExit:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:  # Checks if mouse is pressed
             check_click(event.pos)
+    refresh_screen()
